03553dd58096a006156d324ea9ceeb4e4070da0a
Matthew Pace <pace4@purdue.edu>
2016-03-31 11:01:57 -0400

    Updated data structure to be a pair of (0 indexed line number . beginning index of string on that line) Added inverted-index-from-file.

--- invertedindex.rkt
+++ invertedindex.rkt
@@ -3,21 +3,30 @@
 (require srfi/13)
 
 (define (inverted-index-from-file path)
-  (let ([h (make-hash)])
-    (define (iterate-lines hash lines offset)
-      (let ([current-line (first lines)])
-        (cond
-          [(null? (rest lines)) (inverted-index current-line h offset)]
-          [else (iterate-lines (inverted-index current-line h offset) (rest lines) (+ offset (string-length current-line)))])))
-    (iterate-lines h (file->lines path) 0)))
-  
-; TODO fix start index bug from occurence in previous line (maybe make a pair and keep track of line number and position in line)
-(define (inverted-index text [h (make-hash)] [offset 0])
-  (let ([split (string-split text)])
-    (map (lambda (s)
-           (let* ([list (hash-ref h s [(lambda () empty)])]
-                  [start (if (null? list) 0 (+ (first list) (string-length s)))]
-                  [index (string-contains text s start (string-length text))])
-             (hash-set! h s (cons (+ index offset) list))))
-         split)
-    h))
+  (inverted-index-from-list (file->lines path)))
+
+(define (inverted-index text)
+  (inverted-index-from-list (split-newlines text)))
+
+(define (inverted-index-from-list list [h (make-hash)])
+  (define (iterate-lines hash line-number lines)
+    (let* ([line (first lines)]
+           [split (string-split line)])
+      (cond
+        [(null? (rest lines)) (map ((curry add-to-hash) h line line-number) split)]
+        [else (iterate-lines (map ((curry add-to-hash) h line line-number) split) (+ line-number 1) (rest lines))])))
+  (iterate-lines h 0 list) h)
+
+(define (add-to-hash h line line-number s)
+  (let* ([pair (hash-ref h s [(lambda () empty)])]
+         [start (if (or (null? pair) (not (equal? (car pair) line-number))) 0 (+ (cdr pair) (string-length s)))]
+         [index (string-contains line s start (string-length line))])
+    (hash-set! h s (cons (cons line-number index) pair))) h)
+
+(define (split-newlines text)
+  (define (iterate-string list s)
+    (let ([newline-index (string-index s #\newline)])
+      (cond
+        [(false? newline-index) (cons s list)]
+        [else (iterate-string (cons (substring s 0 newline-index) list) (substring s (+ newline-index 1) (string-length s)))])))
+  (reverse (iterate-string empty text)))
