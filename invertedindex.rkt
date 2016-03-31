217a5f7f6f620d50c0dd76c5ed72a40516b99579
Matthew Pace <pace4@purdue.edu>
2016-03-31 12:09:53 -0400

    Made hash immutable

--- invertedindex.rkt
+++ invertedindex.rkt
@@ -8,20 +8,20 @@
 (define (inverted-index text)
   (inverted-index-from-list (split-newlines text)))
 
-(define (inverted-index-from-list list [h (make-hash)])
+(define (inverted-index-from-list list [h (make-immutable-hash)])
   (define (iterate-lines hash line-number lines)
     (let* ([line (first lines)]
            [split (string-split line)])
       (cond
-        [(null? (rest lines)) (map ((curry add-to-hash) h line line-number) split)]
-        [else (iterate-lines (map ((curry add-to-hash) h line line-number) split) (+ line-number 1) (rest lines))])))
-  (iterate-lines h 0 list) h)
+        [(null? (rest lines)) (foldl ((curry add-to-hash) line line-number) hash split)]
+        [else (iterate-lines (foldl ((curry add-to-hash) line line-number) hash split) (+ line-number 1) (rest lines))])))
+  (iterate-lines h 0 list))
 
-(define (add-to-hash h line line-number s)
+(define (add-to-hash line line-number s h)
   (let* ([pair (hash-ref h s [(lambda () empty)])]
          [start (if (or (null? pair) (not (equal? (car pair) line-number))) 0 (+ (cdr pair) (string-length s)))]
          [index (string-contains line s start (string-length line))])
-    (hash-set! h s (cons (cons line-number index) pair))) h)
+    (hash-set h s (cons (cons line-number index) pair))))
 
 (define (split-newlines text)
   (define (iterate-string list s)
