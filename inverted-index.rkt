35a6d6f3960d67e80b3990e95083b4090989d0f7
Matthew Pace <pace4@purdue.edu>
2016-03-31 12:29:48 -0400

    Better names

---
+++ inverted-index.rkt
@@ -0,0 +1,32 @@
+#lang racket
+
+(require srfi/13)
+
+(define (file->inverted-index path)
+  (list->inverted-index (file->lines path)))
+
+(define (string->inverted-index text)
+  (list->inverted-index (split-newlines text)))
+
+(define (list->inverted-index list [h (make-immutable-hash)])
+  (define (iterate-lines hash line-number lines)
+    (let* ([line (first lines)]
+           [split (string-split line)])
+      (cond
+        [(null? (rest lines)) (foldl ((curry add-to-hash) line line-number) hash split)]
+        [else (iterate-lines (foldl ((curry add-to-hash) line line-number) hash split) (+ line-number 1) (rest lines))])))
+  (iterate-lines h 0 list))
+
+(define (add-to-hash line line-number s h)
+  (let* ([pair (hash-ref h s [(lambda () empty)])]
+         [start (if (or (null? pair) (not (equal? (car pair) line-number))) 0 (+ (cdr pair) (string-length s)))]
+         [index (string-contains line s start (string-length line))])
+    (hash-set h s (cons (cons line-number index) pair))))
+
+(define (split-newlines text)
+  (define (iterate-string list s)
+    (let ([newline-index (string-index s #\newline)])
+      (cond
+        [(false? newline-index) (cons s list)]
+        [else (iterate-string (cons (substring s 0 newline-index) list) (substring s (+ newline-index 1) (string-length s)))])))
+  (reverse (iterate-string empty text)))
